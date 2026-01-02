119879a8b1924cb875d9ce09cb564baa89899e83
Oscar Lozano <srempalador2021@gmail.com>
2026-01-01 21:07:49 -0500

    removiendo comentarios innecesarios

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -16,9 +16,4 @@ export const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 
 /*
-1) claro primero se tiene que autenticar, para luego acceder al dashboard.
-2) token, reportes y usuario.
-3) se cargan una sola vez.
-4) le voy a pasar el repositorio para que pueda ver que llevo.
-5) en el siguiente mensaje le envio código
 */
