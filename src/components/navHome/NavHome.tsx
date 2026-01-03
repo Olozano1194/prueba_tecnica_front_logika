ff69a1b99346f74cf3018b318c63fee6013289e3
Oscar Lozano <srempalador2021@gmail.com>
2026-01-03 15:50:24 -0500

    style: Cambios de estilos en el componente NavHome.

--- src/components/navHome/NavHome.tsx
+++ src/components/navHome/NavHome.tsx
@@ -5,13 +5,13 @@ const NavHome = () => {
   return (
     <TabGroup>
       <TabList className="flex gap-x-20 text-gray-500 font-semibold md:text-lg lg:text-xl">
-        <Tab className="cursor-pointer outline-none data-selected:border-b-2 data-selected:border-violet-950 data-selected:text-violet-950 transition-all">
+        <Tab className="cursor-pointer border-b-2 border-transparent p-2 outline-none data-selected:border-violet-950 data-selected:text-violet-950 transition-all">
           Categorias
         </Tab>
-        <Tab className="cursor-pointer outline-none data-selected:border-b-2 data-selected:border-violet-950 data-selected:text-violet-950 transition-all">
+        <Tab className="cursor-pointer border-b-2 border-transparent p-2 outline-none data-selected:border-violet-950 data-selected:text-violet-950 transition-all">
           Tipos
         </Tab>
-        <Tab className="cursor-pointer outline-none data-selected:border-b-2 data-selected:border-violet-950 data-selected:text-violet-950 transition-all">
+        <Tab className="cursor-pointer border-b-2 border-transparent p-2 outline-none data-selected:border-violet-950 data-selected:text-violet-950 transition-all">
           Filtros
         </Tab>
       </TabList>
@@ -20,10 +20,10 @@ const NavHome = () => {
         <TabPanel className="mt-8">
           <ListHome />
         </TabPanel>
-        <TabPanel className="w-full flex justify-center items-center text-2xl text-violet-950 font-bold">
+        <TabPanel className="w-full flex justify-center items-center mt-8 text-2xl text-violet-950 font-bold">
           Tipos
         </TabPanel>
-        <TabPanel className="w-full flex justify-center items-center text-2xl text-violet-950 font-bold">
+        <TabPanel className="w-full flex justify-center items-center mt-8 text-2xl text-violet-950 font-bold">
           Filtros
         </TabPanel>
       </TabPanels>
